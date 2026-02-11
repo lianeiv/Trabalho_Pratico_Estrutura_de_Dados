8531400378b204bc756b4ede21ebf4a510b20f65
lianeiv <julianeiv4@gmail.com>
2026-02-11 00:54:06 -0300

    Primeira versão das structs

--- structs.h
+++ structs.h
@@ -14,4 +14,31 @@ typedef struct ItemCarrinho {
     struct Produto* produto;//encontrar o produto na lista principal de produtos
     int QuantidadeDesejada;
     struct ItemCarrinho* proximo;//liga ao próximo produto dentro do carrinho
-}
+} ItemCarrinho;
+
+//struct principal para a lista de clientes
+typedef struct Cliente {
+    char cpf [15];
+    char nome [100];
+    char email [100];
+    char telefone [20];
+    DataNascimento dataNasc;
+
+    //conectando ao carrinho
+    struct ItemCarrinho* carrinho;
+    struct Cliente* proximo; 
+} Cliente;
+
+//struct principal para a lista de produtos
+typedef struct Produtos {
+    int codigo;
+    char nome [100];
+    float preco;
+    int quantidade;
+
+    //seguindo para o proximo produto da lista
+    struct Produto* proximo; 
+} Produto;
+
+#endif
+
