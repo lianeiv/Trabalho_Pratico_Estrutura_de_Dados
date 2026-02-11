feec2aba61a6da9dc87441c3ac239b3817f3d3b6
lianeiv <julianeiv4@gmail.com>
2026-02-11 10:30:12 -0300

    Implementação inicial do gerenciamento e main
    Primeira versão da implementação das funções de gerenciamento de produtos e adição da estrutura básica da main

--- funcoes.c
+++ funcoes.c
@@ -7,3 +7,45 @@ void limparBuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
+
+void pausarSistema() {
+    printf("\nPressione Enter para continuar...");
+    getchar();
+}
+
+//funcoes do gerenciamento de produtos
+void cadastrarProduto(Produto** listaProdutos) {
+    Produto* novoProduto = (Produto*) malloc(sizeof(Produto));
+    if (novoProduto == NULL) {
+        printf("Erro: falha na alocação de memória!\n");
+        return;
+    }
+
+    printf("\n--- Cadastro de Novo Produto ---\n");
+    printf("Codigo unico: ");
+    scanf("%d", &novoProduto->codigo);
+    limparBuffer();
+
+    if (buscarProdutoPorCodigo(*listaProdutos, novoProduto->codigo) != NULL) {
+        printf("Erro: Codigo de produto ja existente!\n");
+        free(novoProduto);
+        return;
+    }
+
+    printf("Nome do produto: ");
+    scanf("%99[^\n]", novoProduto->nome);
+    limparBuffer();
+
+    printf("Preco: ");
+    scanf("%f", &novoProduto->preco);
+    limparBuffer();
+
+    printf("Quantidade em estoque: ");
+    scanf("%d", &novoProduto->quantidade);
+    limparBuffer();
+
+    novoProduto->proximo = *listaProdutos;
+    *listaProdutos = novoProduto;
+
+    printf("\nProduto '%s' cadastrado com sucesso!\n", novoProduto->nome);
+}
